2eb6b3cf8b885d2b4a2774a318a29e2405cb4d56
MyDay git <13600769+myday-git@user.noreply.gitee.com>
2023-12-30 13:34:03 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import Antd from 'ant-design-vue'; // 增加
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// 全局引入ant-design插件
+import 'ant-design-vue/dist/antd.css';
+// 引入基本样式
+import './assets/css/reset.less';
 
+Vue.use(Antd); // Vue.use(Button)修改
 Vue.config.productionTip = false;
 
 new Vue({
