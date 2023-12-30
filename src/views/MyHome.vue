e515fad1bae1792d2b061bb197dcd3ec0e20d09b
MyDay git <13600769+myday-git@user.noreply.gitee.com>
2023-12-30 17:48:16 +0800

    菜单栏完成

---
+++ src/views/MyHome.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <!-- 菜单 -->
+    <left-menu />
+    <div :class="{ 'main-app': true, 'menu-unfold': $store.state.collapsed }">
+      <!-- 头部 -->
+      <silder-nav />
+      <!-- 内容区域 -->
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import leftMenu from './components/MyMenu.vue';
+import silderNav from './components/MyHeader.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    leftMenu,
+    silderNav,
+  },
+  methods: {},
+};
+</script>
+<style lang="less">
+@import url(../assets/css/home.less);
+</style>
